deff01b11e0787198b0a05ab886e523e5e4b8c2c
Yersy Aliaga <cognitiva@Cognitivas-MacBook-Pro.local>
2018-06-13 22:04:42 -0500

    cambio

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log('Entramos a la web');
     response.end("Hello World Yersy!");
 
 });
